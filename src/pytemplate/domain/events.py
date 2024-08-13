536330e5bfac3209b75c20682dd87bec005871e4
Rashad Musayev <rmusayevr19@gmail.com>
2024-08-13 10:15:01 +0400

    add TrafficLightChanged event model

--- src/pytemplate/domain/events.py
+++ src/pytemplate/domain/events.py
@@ -1,7 +1,15 @@
 from dataclasses import dataclass
 
+from src.pytemplate.domain.models import TrafficLightState
+
 
 @dataclass
 class VehicleEnteredIntersection:
     vehicle_id: str
     intersection_id: str
+
+
+@dataclass
+class TrafficLightChanged:
+    light_id: str
+    new_state: TrafficLightState
--- tests/test_events.py
+++ tests/test_events.py
@@ -1,4 +1,5 @@
-from src.pytemplate.domain.events import VehicleEnteredIntersection
+from src.pytemplate.domain.events import TrafficLightChanged, VehicleEnteredIntersection
+from src.pytemplate.domain.models import TrafficLightState
 
 
 def test_vehicle_entered_intersection_initialization():
@@ -6,3 +7,10 @@ def test_vehicle_entered_intersection_initialization():
 
     assert event.vehicle_id == "V123"
     assert event.intersection_id == "I456"
+
+
+def test_traffic_light_changed_initialization():
+    event = TrafficLightChanged(light_id="TL123", new_state=TrafficLightState.RED)
+
+    assert event.light_id == "TL123"
+    assert event.new_state == TrafficLightState.RED
